a8dedf689566934593b41f54924acc88637abfba
Muhammadalif1213 <muhammadalif1213@gmail.com>
2025-07-01 13:30:40 +0700

    Membuat service http client untuk fungsi  post.

---
+++ lib/services/services_http_client.dart
@@ -0,0 +1,53 @@
+import 'dart:convert';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:http/http.dart' as http;
+
+class ServicesHttpClient {
+  final String baseUrl = 'http://10.0.2.2:8000/api/';
+  final secureStorage = FlutterSecureStorage();
+
+  //post
+  Future<http.Response> post(String endpoint, Map<String, dynamic> body) async {
+    final url = Uri.parse("$baseUrl$endpoint");
+    try {
+      final response = await http.post(
+        url,
+        headers: {
+          'Accept': 'application/json',
+          'Content-type': 'application/json',
+        },
+        body: jsonEncode(body),
+      );
+      return response;
+    } catch (e) {
+      throw Exception("Post request failed: $e");
+    }
+  }
+
+  //post WITH TOKEN
+  Future<http.Response> postWithToken(
+    String endpoint,
+    Map<String, dynamic> body,
+  ) async {
+    final token = await secureStorage.read(key: "authToken");
+    final url = Uri.parse("$baseUrl$endpoint");
+    try {
+      final response = await http.post(
+        url,
+        headers: {
+          'Authorization': 'Bearer $token',
+          'Accept': 'application/json',
+          'Content-type': 'application/json',
+        },
+        body: jsonEncode(body),
+      );
+      return response;
+    } catch (e) {
+      throw Exception("Post request failed: $e");
+    }
+  }
+
+  //put
+  //delete
+}
